4dcb241d391d65f0dc4d765a4152551ca96790c5
David Amador <solucionaticos@gmail.com>
2024-06-13 09:00:23 -0500

    Fix routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\StoreUserController;
 use App\Http\Controllers\Api\GetUsersController;
 
@@ -21,11 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('users')->group(function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::post('/', [UserController::class, 'store']);
-});
-
 Route::post('/user/create', StoreUserController::class);
-
 Route::get('/user/get-all', GetUsersController::class);
